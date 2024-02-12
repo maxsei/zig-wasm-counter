5d4350a826aae0b1fb28bf9f27b8603990c6b10d
Max Schulte <maximilliangschulte@pm.me>
2024-02-12 11:13:25 -0600

    multiple counters and fixed problem where undefined property "this.pointer" was being passed as a nil pointer to zig

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,63 @@
-import { stream, fromPromise, sync } from "@thi.ng/rstream";
-import { $compile } from "@thi.ng/rdom";
+import { stream, fromPromise } from "@thi.ng/rstream";
+import { $compile, $replace } from "@thi.ng/rdom";
+import { scan, comp, reducer, map } from "@thi.ng/transducers";
 
-const fns = fromPromise(
-  fetch("/zig-out/lib/zig-wasm-triangle.wasm")
-    .then((response) => response.arrayBuffer())
-    .then((bytes) => WebAssembly.compile(bytes))
-    .then((module) => new WebAssembly.Instance(module))
-    .then((instance) => instance.exports),
-);
+const CounterPromise = fetch("/zig-out/lib/zig-wasm-triangle.wasm")
+  .then((response) => response.arrayBuffer())
+  .then((bytes) => WebAssembly.compile(bytes))
+  .then((module) => new WebAssembly.Instance(module))
+  .then(
+    (instance) =>
+      class Counter {
+        constructor() {
+          this._fns = instance.exports;
+          this.self = instance.exports.init();
+        }
+        increment() {
+          this._fns.increment(this.self);
+        }
+        get_count() {
+          return this._fns.get_count(this.self);
+        }
+        deinit() {
+          return this._fns.deinit(this.self);
+        }
+      },
+  );
 
-const click = stream<void>();
+const clickComponent = () =>
+  fromPromise(
+    (async () => {
+      const Counter = await CounterPromise;
+      const counter = new Counter();
+      // TODO: onunmount destroy <12-02-24, Max Schulte> //
+      const click = stream<void>();
+      return [
+        "div",
+        { onclick: () => click.next() },
+        ["div.pointer", {}, "clickme"],
+        click.transform(
+          comp(
+            scan(
+              reducer(
+                () => counter,
+                (acc, cur) => {
+                  acc.increment();
+                  return acc;
+                },
+              ),
+            ),
+            map((x) => x.get_count()),
+          ),
+        ),
+      ];
+    })(),
+  );
+
 const root = document.getElementById("app")!;
 $compile([
   "div",
-  { onclick: () => click.next() },
-  ["div.pointer", {}, "clickme"],
-  sync({ src: { fns, click } }).map(({ fns }) => fns.counter()),
+  {},
+  $replace(clickComponent()),
+  $replace(clickComponent()),
 ]).mount(root);
--- zig/main.zig
+++ zig/main.zig
@@ -1,16 +1,26 @@
 const std = @import("std");
 const testing = std.testing;
 
-var count: ?i32 = null;
-pub export fn counter() i32 {
-    if (count == null) {
-        count = 0;
-        return 0;
-    }
-    count.? += 1;
-    return count.?;
+var __buf: [4096]u8 = undefined;
+var fba = std.heap.FixedBufferAllocator.init(&__buf);
+var allocator = fba.allocator();
+
+pub export fn init() *const Counter {
+    var ret = allocator.create(Counter) catch unreachable;
+    ret.* = Counter{};
+    return ret;
 }
 
-pub export fn myadd(a: f32, b: f32) f32 {
-    return a + b;
-}
+const Counter = struct {
+    count: i32 = 0,
+
+    pub export fn increment(self: *Counter) void {
+        self.count += 1;
+    }
+    pub export fn get_count(self: *Counter) i32 {
+        return self.count;
+    }
+    pub export fn deinit(self: *Counter) void {
+        allocator.destroy(self);
+    }
+};
